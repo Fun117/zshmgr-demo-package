092b25ff6845955fa2d0dc2bdcd56dba225519d2
Fun117 <fun117.kun@gmail.com>
2024-05-16 19:30:34 +0900

    First commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# インストールディレクトリ
+INSTALL_DIR="/usr/local/bin"
+
+# 実行可能ファイルをコピー
+echo "Installing demo.sh to $INSTALL_DIR..."
+sudo cp bin/demo.sh $INSTALL_DIR/demo
+sudo chmod +x $INSTALL_DIR/demo
+echo "zshmgr-demo-package has been installed. You can now use the 'demo' command."
