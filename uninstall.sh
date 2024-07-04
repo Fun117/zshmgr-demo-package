cf50e44ff65ab91945719373fa03374c3be2520a
Fun117 <fun117.kun@gmail.com>
2024-07-04 16:39:02 +0900

    Version update

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+# Detect OS
+OS=$(uname -s)
+
 # Move to the directory of the script
 cd "$(dirname "$0")"
 
 # Installation directory
-INSTALL_DIR="/usr/local/bin"
+case $OS in
+Linux* | Darwin*)
+  INSTALL_DIR="/usr/local/bin"
+  ;;
+MINGW* | MSYS* | CYGWIN*)
+  INSTALL_DIR="/usr/bin"
+  ;;
+*)
+  echo "Unsupported OS: $OS"
+  exit 1
+  ;;
+esac
 
 # Copy executable file
 echo "Uninstalling demo.sh to $INSTALL_DIR..."
